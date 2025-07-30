687a88ba4918e23eff75dca1f0b700f54e3d826b
김보니 <gimboni@AL02405364.local>
2025-07-30 15:05:17 +0900

    Update app.py to handle React asset paths for Streamlit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 절대 경로로 설정
+  base: './', // 상대 경로로 설정
 })
