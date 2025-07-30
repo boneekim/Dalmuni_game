34802d14174f5ea62f55b02b9bf099339ab177e2
김보니 <gimboni@AL02405364.local>
2025-07-30 14:58:03 +0900

    Revert vite.config.ts base to / for Streamlit MIME type issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 상대 경로로 설정
+  base: '/', // 절대 경로로 설정
 })
